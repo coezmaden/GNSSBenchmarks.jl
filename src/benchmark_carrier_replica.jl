48148f94b2d2f2c3220247f1e7984f906123f63c
Can Ozmaden <cozmaden@gmail.com>
2020-10-17 10:40:52 +0200

    Carrier replica benchmark overhaul, min med and mean estimators saved

--- src/benchmark_carrier_replica.jl
+++ src/benchmark_carrier_replica.jl
@@ -1,54 +1,82 @@
 function benchmark_carrier_replica()
-    #init signals
-    cpucarrier = Array(zeros(Complex{Float32}, MAX_NUM_SAMPLES))
-    scpucarrier = StructArray{ComplexF32}((real(cpucarrier),imag(cpucarrier)))
-    gpucarrier = CuArray{ComplexF32}(cpucarrier)
-    sgpucarrier = StructArray{ComplexF32}((real(gpucarrier),imag(gpucarrier)))
-    results = DataFrame(
+    results_min = DataFrame(
         Samples = SAMPLES, 
-        sCPU_median = zeros(Float32,length(SAMPLES)),
-        GPU_median = zeros(Float32,length(SAMPLES)),
-        sGPU_median = zeros(Float32,length(SAMPLES))
+        sCPU_time = zeros(Float64,length(SAMPLES)),
+        GPU_time = zeros(Float64,length(SAMPLES)),
+        sGPU_time = zeros(Float64,length(SAMPLES)),
     )
-    counter = Int32(1)
+    results_med = DataFrame(
+        Samples = SAMPLES, 
+        sCPU_time = zeros(Float64,length(SAMPLES)),
+        GPU_time = zeros(Float64,length(SAMPLES)),
+        sGPU_time = zeros(Float64,length(SAMPLES)),
+    )
+    results_mean = DataFrame(
+        Samples = SAMPLES, 
+        sCPU_time = zeros(Float64,length(SAMPLES)),
+        GPU_time = zeros(Float64,length(SAMPLES)),
+        sGPU_time = zeros(Float64,length(SAMPLES)),
+    )
+    rowpos = Int32(1)
     for N in SAMPLES
-        println("Benchmarking carrier_replica on CPU: Array{ComplexF32} ", N," samples...")
-        result = 1000 / median(@benchmark gpu_gen_carrier_replica!(
-            $cpucarrier[1:$N],
+        # init signals
+        cpucarrier = zeros(Complex{Float32}, N)
+        gpucarrier = CuArray{ComplexF32}(cpucarrier)
+
+        # init signals in StructArrays
+        scpucarrier = StructArray{ComplexF32}(
+            (real(cpucarrier),imag(cpucarrier))
+        )
+        sgpucarrier = StructArray{ComplexF32}(
+            (real(gpucarrier),imag(gpucarrier))
+        )
+
+        # do the benchmark
+        println("Benchmarking carrier_replica on CPU: StructArray{ComplexF32} ", N," samples, ", M," antenna...")
+        result = @benchmark cpu_gen_carrier_replica!(
+            $scpucarrier,
             1500,
             2.5e6,
             0,
             Val(7),
             1,
             $N
-        )).time
-        println(result)
-        results.sCPU_median[counter] = result
-        println("Benchmarking carrier_replica on GPU: CuArray{ComplexF32} ", N, " samples...")
-        result = 1000 / median(@benchmark gpu_gen_carrier_replica!(
-            $gpucarrier[1:$N],
+        )
+        println(minimum(result).time)
+        results_min.sCPU_time[rowpos] = minimum(result).time
+        results_med.sCPU_time[rowpos] = median(result).time
+        results_mean.sCPU_time[rowpos] = mean(result).time
+        println("Benchmarking carrier_replica on GPU: CuArray{ComplexF32} ", N, " samples, ", M," antenna...")
+        result = @benchmark CUDA.@sync gpu_gen_carrier_replica!(
+            $gpucarrier,
             1500,
             2.5e6,
             0,
             Val(7),
             1,
             $N
-        )).time
-        println(result)
-        results.GPU_median[counter] = result
-        println("Benchmarking carrier_replica on GPU: StructArray{ComplexF32}(CuArray,CuArray) ", N, " samples...")
-        result = 1000 / median(@benchmark gpu_gen_carrier_replica!(
-            $sgpucarrier[1:$N],
+        )
+        println(minimum(result).time)
+        results_min.GPU_time[rowpos] = minimum(result).time
+        results_med.GPU_time[rowpos] = median(result).time
+        results_mean.GPU_time[rowpos] = mean(result).time
+        println("Benchmarking carrier_replica on GPU: StructArray{ComplexF32}(CuArray,CuArray) ", N, " samples, ", M," antenna...")
+        result = @benchmark CUDA.@sync gpu_gen_carrier_replica!(
+            $sgpucarrier,
             1500,
             2.5e6,
             0,
             Val(7),
             1,
             $N
-        )).time
-        println(result)
-        results.sGPU_median[counter] = result
-        counter += 1
+        )
+        println(minimum(result).time)
+        results_min.sGPU_time[rowpos] = minimum(result).time
+        results_med.sGPU_time[rowpos] = median(result).time
+        results_mean.sGPU_time[rowpos] = mean(result).time
+        rowpos += 1
     end
-    CSV.write("data/carrierreplica.csv", results)
+    CSV.write("data/carrierreplica_min.csv", results_min)
+    CSV.write("data/carrierreplica_med.csv", results_med)
+    CSV.write("data/carrierreplica_mean.csv", results_mean)
 end
--- src/gpu_carrier_replica.jl
+++ src/gpu_carrier_replica.jl
@@ -1,20 +1,4 @@
-# StructArray of CuArrays
-function gpu_gen_carrier_replica!(
-    carrier_replica::StructArray{ComplexF32},
-    carrier_frequency,
-    sampling_frequency,
-    start_phase,
-    carrier_amplitude_power,
-    start_sample,
-    num_samples
-)
-    @. carrier_replica.re = 2pi * (1:num_samples) * carrier_frequency / sampling_frequency + start_phase
-    @. carrier_replica.im = sin(carrier_replica.re)
-    @. carrier_replica.re = cos(carrier_replica.re)
-    return carrier_replica
-end
-
-# CuArray
+# GPU CuArray
 function gpu_gen_carrier_replica!(
     carrier_replica::CuArray{ComplexF32},
     carrier_frequency,
@@ -27,16 +11,40 @@ function gpu_gen_carrier_replica!(
     @. @views carrier_replica[start_sample:num_samples] = cis(2pi * (1:num_samples) * carrier_frequency / sampling_frequency + start_phase)
 end
 
-# Float32 implementation of the orig. CPU function
+# GPU StructArray of CuArrays
 function gpu_gen_carrier_replica!(
-    carrier_replica::Array{ComplexF32},
+    carrier_replica::StructOfCuVectors,
     carrier_frequency,
     sampling_frequency,
     start_phase,
     carrier_amplitude_power,
     start_sample,
     num_samples
-)
-    @. carrier_replica = cis(2pi * (1:num_samples) * carrier_frequency / sampling_frequency + start_phase)
+)where{
+    StructOfCuVectors <: StructArray{Complex{Float32},1,NamedTuple{(:re, :im),Tuple{CuArray{Float32,1},CuArray{Float32,1}}},Int64}
+}
+    @. carrier_replica.re = 2pi * (1:num_samples) * carrier_frequency / sampling_frequency + start_phase
+    @. carrier_replica.im = sin(carrier_replica.re)
+    @. carrier_replica.re = cos(carrier_replica.re)
+    return carrier_replica
+end
+
+# CPU StructArray 
+function cpu_gen_carrier_replica!(
+    carrier_replica::StructOfVectors,
+    carrier_frequency,
+    sampling_frequency,
+    start_phase,
+    carrier_amplitude_power,
+    start_sample,
+    num_samples
+) where {
+    StructOfVectors <: StructArray{Complex{Float32},1,NamedTuple{(:re, :im),Tuple{Array{Float32,1},Array{Float32,1}}},Int64}
+}
+    @. carrier_replica.re = 2pi * (1:num_samples) * carrier_frequency / sampling_frequency + start_phase
+    @avx unroll = 6 for i = start_sample:num_samples + start_sample - 1
+        carrier_replica.im[i] = sin(carrier_replica.re[i])
+        carrier_replica.re[i] = cos(carrier_replica.re[i])
+    end
     return carrier_replica
 end
